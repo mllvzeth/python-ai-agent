d1847118a509ff60b6feeede88462e7eeff8efbb
mllvzeth <1093667+mllvzeth@users.noreply.github.com>
2026-01-12 13:56:51 -0800

    feat: initial project setup with calculator and file utilities

---
+++ functions/get_files_info.py
@@ -0,0 +1,27 @@
+import os
+
+def get_files_info(working_directory, directory="."):
+    working_dir_abs = os.path.abspath(working_directory)
+    target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
+    valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+
+    if  not valid_target_dir:
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    if not os.path.isdir(target_dir):
+        return f'Error: "{directory}" is not a directory'
+
+    files_list = []
+
+    try:
+
+        with os.scandir(target_dir) as entries:
+            for entry in entries:
+                size = os.path.getsize(entry.path)
+                check_dir = entry.is_dir()
+                files_list.append(f'- {entry.name}: file_size={size} bytes, is_dir={check_dir}')
+        return '\n'.join(files_list)
+
+    except Exception as e:
+        print(f'Error: {e}') 
+
+
---
+++ main.py
@@ -0,0 +1,53 @@
+import os, time
+from dotenv import load_dotenv
+from google import genai
+from google.genai import errors, types
+import argparse
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+
+if api_key is None:
+    raise RuntimeError("GEMINI_API_KEY not found in environment variables")
+
+client = genai.Client(api_key=api_key)
+
+def main():
+
+    print("Hello from python-ai-agent!")
+
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+    # Now we can access `args.user_prompt`
+
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    if args.verbose:
+        print(f"User prompt: {messages}")
+
+
+    for attempt in range(5):
+        try:
+            response = client.models.generate_content(
+                model='gemini-2.5-flash',
+                contents=messages
+            )
+
+            if response.usage_metadata is None: raise RuntimeError("Usage data missing")
+            if args.verbose:
+                print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+                print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+            print(f"Response: {response.text}")
+            break
+        except errors.ServerError as e:
+            print(f"Attempt {attempt+1} failed: {e}")
+            if attempt == 4:
+                raise
+            time.sleep(2 * (attempt + 1))  # simple backoff
+
+
+if __name__ == "__main__":
+    main()
