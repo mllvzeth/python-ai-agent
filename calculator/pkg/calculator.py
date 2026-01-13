933cfa56194c20b0b21858390f62824de1d85382
mllvzeth <1093667+mllvzeth@users.noreply.github.com>
2026-01-13 15:14:47 -0800

    feat: reading files

--- calculator/pkg/calculator.py
+++ calculator/pkg/calculator.py
@@ -58,4 +58,4 @@ class Calculator:
 
         b = values.pop()
         a = values.pop()
-        values.append(self.operators[operator](a, b))
+        values.append(self.operators[operator](a, b)e
