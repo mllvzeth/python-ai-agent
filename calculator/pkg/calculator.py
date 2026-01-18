2c1dfcbac3cbc460be83cab1e1746548bd69ed32
mllvzeth <1093667+mllvzeth@users.noreply.github.com>
2026-01-17 18:54:08 -0800

    feat: added system prompt

--- calculator/pkg/calculator.py
+++ calculator/pkg/calculator.py
@@ -58,4 +58,4 @@ class Calculator:
 
         b = values.pop()
         a = values.pop()
-        values.append(self.operators[operator](a, b)e
+        values.append(self.operators[operator](a, b))
