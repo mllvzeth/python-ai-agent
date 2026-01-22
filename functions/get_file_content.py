0cd52cfbecfffbe07d205de0a9471e438396fce4
mllvzeth <1093667+mllvzeth@users.noreply.github.com>
2026-01-21 16:57:39 -0800

    update: working on calling functions
    commit-id:6ec27fad

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,25 +1,46 @@
 import os
 from config import MAX_CHARS
+from google.genai import types
+
+
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description="Gets the contents of a file",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="File path to get file info from, relative to the working directory",
+            ),
+        },
+        required=["file_path"],
+    ),
+)
+
 
 def get_file_content(working_directory, file_path):
-
     working_dir_abs = os.path.abspath(working_directory)
     target_file = os.path.normpath(os.path.join(working_dir_abs, file_path))
-    valid_target_file = os.path.commonpath([working_dir_abs, target_file]) == working_dir_abs
+    valid_target_file = (
+        os.path.commonpath([working_dir_abs, target_file]) == working_dir_abs
+    )
 
     if not valid_target_file:
         return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
-    
+
     if not os.path.isfile(target_file):
         return f'Error: File not found or is not a regular file: "{file_path}"'
 
     try:
-        with open(target_file, 'r') as f:
+        with open(target_file, "r") as f:
             content = f.read(MAX_CHARS)
             if f.read(1):
-                content += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+                content += (
+                    f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+                )
 
         return content
 
     except Exception as e:
-        print(f'Error: {e}')
+        print(f"Error: {e}")
--- functions/write_file.py
+++ functions/write_file.py
@@ -1,11 +1,32 @@
 import os
-from config import MAX_CHARS
+from google.genai import types
+
+schema_write_file = types.FunctionDeclaration(
+    name="write_file",
+    description="Writes to a file",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="File path to write to, relative to the working directory",
+            ),
+            "content": types.Schema(
+                type=types.Type.STRING,
+                description="The content to write to the file",
+            ),
+        },
+        required=["file_path"],
+    ),
+)
+
 
 def write_file(working_directory, file_path, content):
-
     working_dir_abs = os.path.abspath(working_directory)
     target_file = os.path.normpath(os.path.join(working_dir_abs, file_path))
-    valid_target_file = os.path.commonpath([working_dir_abs, target_file]) == working_dir_abs
+    valid_target_file = (
+        os.path.commonpath([working_dir_abs, target_file]) == working_dir_abs
+    )
 
     if not valid_target_file:
         return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
@@ -14,13 +35,14 @@ def write_file(working_directory, file_path, content):
         return f'Error: Cannot write to "{file_path}" as it is a directory'
 
     try:
-
         os.makedirs(working_dir_abs, mode=0o777, exist_ok=True)
 
-        with open(target_file, 'w') as f:
+        with open(target_file, "w") as f:
             f.write(content)
 
-        return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+        return (
+            f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+        )
 
     except Exception as e:
-        print(f'Error: {e}')
+        print(f"Error: {e}")
